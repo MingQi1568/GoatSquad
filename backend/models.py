229c34c02aca353b19de581fce3d3b9f4df0a57c
ccalvinhuang <ccalvinhhuang@gmail.com>
2025-01-20 00:05:05 -0500

    cleanup

--- backend/models.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timezone
-
-class User(db.Model):
-    # ...
-    created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
-    updated_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc), 
-                          onupdate=lambda: datetime.now(timezone.utc)) 
