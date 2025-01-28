f365f6d1bc85480402a4cddf68e489561f7c4402
ccalvinhuang <ccalvinhhuang@gmail.com>
2025-01-28 00:32:18 -0500

    gemini embedding generation

--- backend/gemini.py
+++ backend/gemini.py
@@ -3,6 +3,9 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 import logging
+import google.generativeai as genai
+import os
+
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -32,3 +35,22 @@ def run_gemini_prompt(prompt):
     except Exception as e:
         logger.error(f"Error running Gemini prompt: {str(e)}", exc_info=True)
         return None
+
+def generate_embeddings(text):
+    try:
+        logger.info(f"Generating embeddings for text: {text[:50]}...")
+
+        genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
+        result = genai.embed_content(
+                model="models/text-embedding-004",
+                content="What is the meaning of life?")
+
+        embedded = str(result['embedding'])
+        logger.info(f"Embeddings generated successfully: {embedded}...")
+        return embedded
+    except Exception as e:
+        logger.error(f"Error generating embeddings: {str(e)}", exc_info=True)
+        return None
+
+
+
