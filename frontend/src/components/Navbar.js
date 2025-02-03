b055aa22c81357da7c8b3c35f5efff8c632634f3
Andy Xu <xuandy05@gmail.com>
2025-02-02 18:45:41 -0800

    edits

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -46,7 +46,7 @@ function Navbar() {
     { path: '/calendar', label: 'Calendar' },
     { path: '/preferences', label: 'Preferences' },
     { path: '/recommendations', label: 'Recommendations' },
-    { path: '/showcase-compilation', label: 'Showcase' },
+    { path: '/showcase-compilation', label: 'Highlight' },
     { path: '/saved-videos', label: 'Saved Videos' }
   ];
 
